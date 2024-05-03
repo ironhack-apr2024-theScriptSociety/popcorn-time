9026cb2cbd787c8d21555a9c1a9b4ca2f5366e4f
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-05-03 10:59:18 +0200

    forms: add form with 1 field

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,59 @@ function App() {
 
   const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
+  const [title, setTitle] = useState("");
+
+
   const deleteMovie = (movieId) => {
     const newList = moviesToDisplay.filter(movie => movie.id !== movieId);
     setMoviesToDisplay(newList);
   }
 
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const newMovie = {
+      title: title,
+      year: 2001
+    }
+
+    // moviesToDisplay.push(newMovie); // NEVER, NEVER, modify state directly !!
+
+    const newList = [newMovie, ...moviesToDisplay];
+
+    setMoviesToDisplay(newList);
+
+    // Clear form
+    setTitle("");
+
+  }
+
+  
   return (
     <>
     
       <Header numberOfMovies={moviesToDisplay.length} />
+
+      <section>
+        <h2>Create your own movie</h2>
+
+        <form onSubmit={handleSubmit} >
+
+          <label>Title:
+            <input 
+              type="text" 
+              name='title' 
+              placeholder='enter the title' 
+              value={title} 
+              onChange={(e) => { setTitle(e.target.value) }}
+            />
+          </label>
+
+          <p><button>Create</button></p>
+        </form>
+
+      </section>
 
       <Routes>
         <Route path="/" element={<MovieList moviesToDisplay={moviesToDisplay} callbackToDelete={deleteMovie} />} />
